ddcdfbd7d218c53a81fe2b8898ce16afa9d68919
Emily M <30880308+e0marina@users.noreply.github.com>
2020-05-15 20:25:30 -0700

    added sidenav component and included bootstrap cdn in index.html

---
+++ client/src/components/SideNav/sideNav.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "./style.css";
+
+export function SideNav(props) {
+  return (
+    <div>
+      <div className="wrapper">
+        <nav id="sidebar">
+          <div className="sidebar-header">
+            <h3>Area where profile card and logo go</h3>
+          </div>
+
+          <ul className="list-unstyled components">
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">Profile</a>
+            </li>
+            <li>
+              <a href="#">Logout</a>
+            </li>
+            <li>
+              <a href="#"></a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  );
+}
+export default SideNav;
