63ea0ce65f70152e2b300531dee996e530447d78
Emily M <30880308+e0marina@users.noreply.github.com>
2020-05-17 20:43:15 -0700

    just trying to get out of herre

--- client/src/pages/signInHome.js
+++ client/src/pages/signInHome.js
@@ -1,13 +1,22 @@
 import React, { Component } from "react";
 import Wrapper from "../components/Wrapper/wrapper";
-import ResponsiveNav from "../responsiveNav";
+import TopNav from "../components/TopNav/topNav";
+import PostCard from "../components/PostCard/postCard";
+import SignIn from "../components/SignIn/signIn";
 
 class signInHome extends Component {
   render() {
     return (
-      <div className="home">
-        <Wrapper />
-        <ResponsiveNav />
+      <div className="App">
+        <div className="row">
+          <div class="col-sm-8">
+            <sign_in></sign_in>
+            <TopNav /> <PostCard />
+          </div>
+          <div class="col-sm-4">
+            <SignIn />
+          </div>
+        </div>
       </div>
     );
   }
