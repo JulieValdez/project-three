4c29e38e2adf073205f3f02686e41bd62618a1fb
Emily M <30880308+e0marina@users.noreply.github.com>
2020-05-18 20:05:58 -0700

    now modal renders yay

---
+++ client/src/components/PopUp/popUpToggle.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Button from "react-bootstrap/Button";
+import PopUp from "./popUp";
+
+function PopUpToggle() {
+  const [modalShow, setModalShow] = React.useState(false);
+
+  return (
+    <>
+      <Button variant="primary" onClick={() => setModalShow(true)}>
+        Create a Post
+      </Button>
+
+      <PopUp show={modalShow} onHide={() => setModalShow(false)} />
+    </>
+  );
+}
+
+export default PopUpToggle;
--- client/src/pages/userHome.js
+++ client/src/pages/userHome.js
@@ -1,29 +1,15 @@
 import React, { Component } from "react";
 import Wrapper from "../components/Wrapper/wrapper";
 import ResponsiveNav from "../responsiveNav";
-import PopUp from "../components/PopUp/popUp";
-import Button from "react-bootstrap/Button";
+import PopUpToggle from "../components/PopUp/popUpToggle";
 
 class userHome extends Component {
-  ModalShowHide = () => {
-    const [modalShow, setModalShow] = React.useState(false);
-
-    return (
-      <>
-        <Button variant="primary" onClick={() => setModalShow(true)}>
-          Launch vertically centered modal
-        </Button>
-
-        <PopUp show={modalShow} onHide={() => setModalShow(false)} />
-      </>
-    );
-  };
   render() {
     return (
       <div className="App">
         <Wrapper />
         <ResponsiveNav />
-        <PopUp />
+        <PopUpToggle />
       </div>
     );
   }
