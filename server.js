ea65a97f0bf7fd1486b5442baa0346978fa2c7e2
JulieValdez <juliegragg0605@gmail.com>
2020-05-31 18:44:40 -0700

    minor changes

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const path = require("path");
 const mongoose = require("mongoose");
 const PORT = process.env.PORT || 3001;
 const app = express();
+require("dotenv").config();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
