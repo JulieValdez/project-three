b12d20a149430b1d1bd9bdd0c00a9cc01e8a4226
Emily M <30880308+e0marina@users.noreply.github.com>
2020-06-02 18:10:14 -0700

    changed to client/build in server.js file

---
+++ client/build/precache-manifest.2744f8d19a09a2ade7e0a5a554310fc4.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "a66a2ba6288b0271fec6",
+    "url": "/static/css/main.6ff6f7e0.chunk.css"
+  },
+  {
+    "revision": "a66a2ba6288b0271fec6",
+    "url": "/static/js/main.9d827d54.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "803bb910cc0a8f21d798",
+    "url": "/static/css/2.dccfc8b8.chunk.css"
+  },
+  {
+    "revision": "803bb910cc0a8f21d798",
+    "url": "/static/js/2.f2410fb6.chunk.js"
+  },
+  {
+    "revision": "a56bb68be53aee3a5b4e8a08b848e356",
+    "url": "/static/media/profilepic.a56bb68b.jpeg"
+  },
+  {
+    "revision": "d289725718cc0db8a71e991830932f63",
+    "url": "/static/media/signinGraphic.d2897257.svg"
+  },
+  {
+    "revision": "3832128181b7bbe8e97646df8d2b715e",
+    "url": "/static/media/six.38321281.png"
+  },
+  {
+    "revision": "5d6653a70b39b7b9cf07392a3cb07612",
+    "url": "/static/media/registerGraphic.5d6653a7.svg"
+  },
+  {
+    "revision": "7a662552ed5370729e03e3fd33ea329d",
+    "url": "/index.html"
+  }
+];
